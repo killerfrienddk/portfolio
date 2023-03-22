6bdef98a70bf2aa6244922bebd53b07679b7c2da
Simon Hjorthøj <sh2@live.dk>
2023-03-22 14:25:25 +0100

    Removed font awesome since I no longer use it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,6 @@ export default {
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [
-        {src: '~/plugins/font-awesome.ts', mode: 'client'},
         {src: '~/plugins/constants.ts'},
         {src: '~/plugins/guid.ts'},
     ],
